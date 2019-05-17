0df98073f7285dcab47d2a9d6e52d86afb763e3c
Ya-Liang Chang (Allen) <yaliangchang@cmlab.csie.ntu.edu.tw>
2019-05-17 08:06:51 +0000

    Add face recognition model by backbone and head

---
+++ src/model/backbone/__init__.py
@@ -0,0 +1,4 @@
+from . import *  # NOQA
+from .mobilenetv2 import MobileNetV2
+
+MobileNetV2 = MobileNetV2
--- src/model/model.py
+++ src/model/model.py
@@ -1,32 +1,19 @@
-import torch.nn as nn
-import torch.nn.functional as F
-
 from base import BaseModel
-from model.mobilenetv2 import MobileNetV2
+import model.backbone as backbones
+import model.head as heads
 
 
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
-        super(MnistModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+class FaceRecognitionModel(BaseModel):
+    def __init__(self, backbone, head, n_class=1000, embedding_size=512, input_size=112, device_id=[0], **kwargs):
+        super().__init__()
+        self.backbone = getattr(backbones, backbone)(embedding_size, input_size)
+        self.head = getattr(heads, head)(in_features=embedding_size, out_features=n_class, device_id=device_id)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
-
-class MobileNetV2Model(BaseModel):
-    def __init__(self, n_class=1000, input_size=112, width_mult=1.):
-        self.model = MobileNetV2(n_class, input_size, width_mult)
+    def get_features(self, x):
+        features = self.backbone(x)
+        return features
 
     def forward(self, x):
-        return self.model(x)
+        features = self.get_features(x)
+        out = self.head(features)
+        return out
