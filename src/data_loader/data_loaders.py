083bdf9bbbad2d0a8a554adff686aacc647012a4
Ya-Liang Chang (Allen) <yaliangchang@cmlab.csie.ntu.edu.tw>
2019-05-17 03:56:29 +0000

    Add LFW dataloader

--- src/data_loader/data_loaders.py
+++ src/data_loader/data_loaders.py
@@ -14,3 +14,26 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super(MnistDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class LFWDataLoader(BaseDataLoader):
+    """
+    LFW data loader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=4, training=True):
+        if training:
+            self.transform = transforms.Compose([
+                transforms.Resize((112, 112)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.Resize((112, 112)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ])
+        self.data_dir = data_dir
+        self.dataset = datasets.ImageFolder(self.data_dir, transform=self.transform)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
