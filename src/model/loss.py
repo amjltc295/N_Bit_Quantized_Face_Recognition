9690441971cdd1f91eba977ab917133617211229
Ya-Liang Chang (Allen) <yaliangchang@cmlab.csie.ntu.edu.tw>
2019-05-17 04:18:32 +0000

    Add CrossEntropyLoss

--- src/model/loss.py
+++ src/model/loss.py
@@ -1,5 +1,10 @@
-import torch.nn.functional as F
+from torch import nn
 
 
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
+class CrossEntropyLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, output, target):
+        return self.loss_fn(output, target)
