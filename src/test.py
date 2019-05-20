9f5126a9b4f6f4e0856adc2c9ddd6a4f0833c829
Ya-Liang Chang (Allen) <yaliangchang@cmlab.csie.ntu.edu.tw>
2019-05-20 04:18:49 +0000

    Update test.py to test quantized inference

--- src/test.py
+++ src/test.py
@@ -3,23 +3,15 @@ import torch
 from tqdm import tqdm
 import data_loader.data_loaders as module_data
 import model.loss as module_loss
-import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
 
 
-def main(config, resume):
+def main(config):
     logger = config.get_logger('test')
 
     # setup data_loader instances
-    data_loader = getattr(module_data, config['data_loader']['type'])(
-        config['data_loader']['args']['data_dir'],
-        batch_size=512,
-        shuffle=False,
-        validation_split=0.0,
-        training=False,
-        num_workers=2
-    )
+    data_loader = config.initialize('data_loader', module_data)
 
     # build model architecture
     model = config.initialize('arch', module_arch)
@@ -27,10 +19,9 @@ def main(config, resume):
 
     # get function handles of loss and metrics
     loss_fn = getattr(module_loss, config['loss'])
-    metric_fns = [getattr(module_metric, met) for met in config['metrics']]
 
-    logger.info('Loading checkpoint: {} ...'.format(resume))
-    checkpoint = torch.load(resume)
+    logger.info('Loading checkpoint: {} ...'.format(config.resume))
+    checkpoint = torch.load(config.resume)
     state_dict = checkpoint['state_dict']
     if config['n_gpu'] > 1:
         model = torch.nn.DataParallel(model)
@@ -41,31 +32,19 @@ def main(config, resume):
     model = model.to(device)
     model.eval()
 
-    total_loss = 0.0
-    total_metrics = torch.zeros(len(metric_fns))
-
     with torch.no_grad():
         for i, (data, target) in enumerate(tqdm(data_loader)):
+            if i > 0:
+                break
             data, target = data.to(device), target.to(device)
-            output = model(data)
-
-            #
-            # save sample images, or do something with output here
-            #
-
-            # computing loss, metrics on test set
-            loss = loss_fn(output, target)
-            batch_size = data.shape[0]
-            total_loss += loss.item() * batch_size
-            for i, metric in enumerate(metric_fns):
-                total_metrics[i] += metric(output, target) * batch_size
-
-    n_samples = len(data_loader.sampler)
-    log = {'loss': total_loss / n_samples}
-    log.update({
-        met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
-    })
-    logger.info(log)
+            output = model.get_features(data)
+            for n, module in model.named_modules():
+                module.weight = torch.nn.Parameter(module.weight.type(torch.uint8).to(device))
+                if module.bias is not None:
+                    module.bias = torch.nn.Parameter(module.bias.type(torch.uint8).to(device))
+            qout = model.get_features(data)
+            loss = loss_fn(output, qout)
+            logger.info(f"Loss: {loss}")
 
 
 if __name__ == '__main__':
@@ -75,7 +54,9 @@ if __name__ == '__main__':
                         help='path to latest checkpoint (default: None)')
     parser.add_argument('-d', '--device', default=None, type=str,
                         help='indices of GPUs to enable (default: all)')
+    parser.add_argument('-c', '--configs', default=None, type=str,
+                        nargs='+',
+                        help='config file paths (default: None)')
 
-    args = parser.parse_args()
-    config = ConfigParser(args)
-    main(config, args.resume)
+    config = ConfigParser(parser)
+    main(config)
