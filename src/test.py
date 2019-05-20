3a88da37ded46b9f4414cfb67bb80bc4067deab4
Ya-Liang Chang (Allen) <yaliangchang@cmlab.csie.ntu.edu.tw>
2019-05-20 09:07:45 +0000

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,7 +2,6 @@ import argparse
 import torch
 from tqdm import tqdm
 import data_loader.data_loaders as module_data
-import model.loss as module_loss
 import model.model as module_arch
 from parse_config import ConfigParser
 
@@ -14,35 +13,36 @@ def main(config):
     data_loader = config.initialize('data_loader', module_data)
 
     # build model architecture
+    config['arch']['args']['n_class'] = config['data_loader']['args']['n_class']
     model = config.initialize('arch', module_arch)
     logger.info(model)
 
     # get function handles of loss and metrics
-    loss_fn = getattr(module_loss, config['loss'])
+    loss_fn = torch.nn.MSELoss()
 
     logger.info('Loading checkpoint: {} ...'.format(config.resume))
     checkpoint = torch.load(config.resume)
-    state_dict = checkpoint['state_dict']
     if config['n_gpu'] > 1:
         model = torch.nn.DataParallel(model)
-    model.load_state_dict(state_dict)
+
+    model.load_backbone(checkpoint)
 
     # prepare model for testing
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
-    model.eval()
 
     with torch.no_grad():
         for i, (data, target) in enumerate(tqdm(data_loader)):
             if i > 0:
                 break
             data, target = data.to(device), target.to(device)
+            model.train()
             output = model.get_features(data)
-            for n, module in model.named_modules():
-                module.weight = torch.nn.Parameter(module.weight.type(torch.uint8).to(device))
-                if module.bias is not None:
-                    module.bias = torch.nn.Parameter(module.bias.type(torch.uint8).to(device))
+
+            model.eval()
+            # TODO: implement Integer-Arithmetic-Only Inference
             qout = model.get_features(data)
+
             loss = loss_fn(output, qout)
             logger.info(f"Loss: {loss}")
 
